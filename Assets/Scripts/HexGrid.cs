0d15c417bd8af4e59b9b4fc1455371ef5f7cfb90
Pierson <pstrimar@gmail.com>
2023-01-06 12:39:36 -0500

    animating movement

--- Assets/Scripts/HexGrid.cs
+++ Assets/Scripts/HexGrid.cs
@@ -199,6 +199,19 @@ public class HexGrid : MonoBehaviour
         ShowPath(speed);
     }
 
+    public List<HexCell> GetPath()
+    {
+        if (!currentPathExists) return null;
+        List<HexCell> path = ListPool<HexCell>.Get();
+        for (HexCell c = currentPathTo; c != currentPathFrom; c = c.PathFrom)
+        {
+            path.Add(c);
+        }
+        path.Add(currentPathFrom);
+        path.Reverse();
+        return path;
+    }
+
     void ShowPath(int speed)
     {
         if (currentPathExists) 
@@ -206,7 +219,7 @@ public class HexGrid : MonoBehaviour
 			HexCell current = currentPathTo;
 			while (current != currentPathFrom) 
             {
-				int turn = current.Distance / speed;
+				int turn = (current.Distance - 1) / speed;
 				current.SetLabel(turn.ToString());
 				current.EnableHighlight(Color.white);
 				current = current.PathFrom;
@@ -258,7 +271,7 @@ public class HexGrid : MonoBehaviour
                 return true;
             }
 
-            int currentTurn = current.Distance / speed;
+            int currentTurn = (current.Distance - 1) / speed;
 
             for (HexDirection d = HexDirection.NE; d <= HexDirection.NW; d++) 
             {
@@ -292,7 +305,7 @@ public class HexGrid : MonoBehaviour
                 }
                 
                 int distance = current.Distance + moveCost;
-                int turn = distance / speed;
+                int turn = (distance - 1) / speed;
                 if (turn > currentTurn)
                 {
                     distance = turn * speed + moveCost;
